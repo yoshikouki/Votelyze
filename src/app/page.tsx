0c23d5cc5d6e8630008efd540938704f704ad81b
yoshikouki <yoshikouki@pepabo.com>
2025-04-26 10:23:38 +0900

    bunx --bun shadcn@latest init
    format

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
       <main className="row-start-2 flex flex-col items-center gap-[32px] sm:items-start">
-        <h1 className="text-4xl font-bold">Votelyze</h1>
+        <h1 className="font-bold text-4xl">Votelyze</h1>
         <p className="text-lg">Super simple voting app</p>
       </main>
     </div>
