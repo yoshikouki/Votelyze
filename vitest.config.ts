6794edfc67d908a923e66cb17d9e249590c9ba2b
yoshikouki <yoshikouki@pepabo.com>
2025-04-26 11:38:19 +0900

    format

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,6 @@
 import { resolve } from "path";
+import tsconfigPaths from "vite-tsconfig-paths";
 import { defineConfig } from "vitest/config";
-import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
